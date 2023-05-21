e36540668c41e3bb73e5c017f5a17e7b21234b56
JoeLanglands <36076875+JoeLanglands@users.noreply.github.com>
2023-05-21 15:23:40 +0100

    wip: added favicons and edited footer bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/ui-components/navbar";
 import { ThemeProvider } from "@mui/system";
+import { ThemeProvider as ScThemeProvider } from "styled-components";
 import { Routes, Route} from "react-router-dom";
 import theme from "./theme";
 
@@ -8,18 +9,22 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/ui-components/footer";
 
+// Put in horrible double theme provider hack so that styled components can use the theme
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects/>} />
-        </Routes>
-        <Footer/>
-      </div>
+      <ScThemeProvider theme={theme}>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ScThemeProvider>
     </ThemeProvider>
   );
 }
--- src/components/ui-components/footer.tsx
+++ src/components/ui-components/footer.tsx
@@ -3,23 +3,28 @@ import { GitHub, LinkedIn } from "@mui/icons-material";
 import { Link } from "@mui/material";
 
 const FooterContainer = styled.div`
-    text-align: center;
-    position: absolute;
-    bottom: 0;
-    width: 100% !important;
-    margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+  flex-direction: row;
+  position: absolute;
+  bottom: 0;
+  width: 100% !important;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  padding: 10px;
+  right: 0;
 `;
 
 const Footer = () => {
   return (
-      <FooterContainer>
-        <Link href="https://github.com/JoeLanglands">
-            <GitHub color="secondary"/>
-        </Link>
-        <Link href="https://www.linkedin.com/in/joe-langlands-3686891b0/">
-            <LinkedIn color="secondary" />
-        </Link>
-      </FooterContainer>
+    <FooterContainer>
+      <Link href="https://github.com/JoeLanglands">
+        <GitHub color="secondary" />
+      </Link>
+      <Link href="https://www.linkedin.com/in/joe-langlands-3686891b0/">
+        <LinkedIn color="secondary" />
+      </Link>
+    </FooterContainer>
   );
 };
 
