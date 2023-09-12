cb2b2e9139fd4f1a6926819f84f2288395cd2ac8
JoeLanglands <36076875+JoeLanglands@users.noreply.github.com>
2023-09-12 18:44:57 +0100

    wip: save and load site config on exit and handle IPv6 addresses for unique visitors

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,8 @@ package config
 import (
 	"encoding/json"
 	"log/slog"
+	"net"
 	"os"
-	"strings"
 
 	"github.com/JoeLanglands/joes-website/internal/state"
 )
@@ -23,22 +23,38 @@ type SiteConfig struct {
 	UniqueVisitors map[string]struct{}      `json:"unique_visitors"`
 }
 
-func LoadOrNewConfig() *SiteConfig {
+// NewConfig returns a (pointer to) new default SiteConfig
+func NewConfig() *SiteConfig {
 	return &SiteConfig{
 		UniqueVisitors: make(map[string]struct{}),
 	}
+}
+
+func LoadOrNewConfig() *SiteConfig {
+	cfgData, err := os.ReadFile(configFile)
+	if err != nil {
+		slog.Error("Failed to read config file, using defaults", "error", err)
+		return NewConfig()
+	}
+	var cfg SiteConfig
+	err = json.Unmarshal(cfgData, &cfg)
+	if err != nil {
+		slog.Error("Failed to unmarshal config file, using defaults", "error", err)
+		return NewConfig()
+	}
+	return &cfg
 }
 
 func (cfg *SiteConfig) GetUniqueVisitors() int {
 	return len(cfg.UniqueVisitors)
 }
 
-func (cfg *SiteConfig) AddUniqueVisitor(fullIP string) {
-	ip, _, ok := strings.Cut(fullIP, ":")
-	if !ok {
-		cfg.Logger.Info("Failed to cut IP address", "ip", fullIP)
+func (cfg *SiteConfig) AddUniqueVisitor(remoteAddr string) {
+	host, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		cfg.Logger.Error("Failed to split host and port", "error", err)
 	}
-	cfg.UniqueVisitors[ip] = struct{}{}
+	cfg.UniqueVisitors[host] = struct{}{}
 }
 
 func (cfg *SiteConfig) SaveConfig() {
@@ -47,7 +63,7 @@ func (cfg *SiteConfig) SaveConfig() {
 		cfg.Logger.Error("Failed to marshal config", "error", err)
 		return
 	}
-	err = os.WriteFile("site_config.json", cfgData, 0644)
+	err = os.WriteFile(configFile, cfgData, 0644)
 	if err != nil {
 		cfg.Logger.Error("Failed to write config", "error", err)
 	}
